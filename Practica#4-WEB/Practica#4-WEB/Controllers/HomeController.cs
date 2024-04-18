f20b58a6ce91e9794b301a4aab21f470002e1691
davidferga <dfernandez10362@ufide.ac.cr>
2024-04-18 17:24:34 -0600

    Avance WEB
    Avance WEB

--- Practica#4-WEB/Practica#4-WEB/Controllers/HomeController.cs
+++ Practica#4-WEB/Practica#4-WEB/Controllers/HomeController.cs
@@ -1,16 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Practica_4_WEB.Entities;
 using Practica_4_WEB.Models;
 using System.Diagnostics;
+using System.Net.Http;
 
 namespace Practica_4_WEB.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly HttpClient _httpClient;
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+            _httpClient = new HttpClient();
         }
 
         public IActionResult Index()
---
+++ Practica#4-WEB/Practica#4-WEB/Controllers/PrincipalController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Practica_4_API.Entities;
+using System.Net.Http.Json;
+
+namespace Practica_4_Web.Controllers
+{
+    public class PrincipalController : Controller
+    {
+        private readonly HttpClient _httpClient;
+
+        public PrincipalController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClient = httpClientFactory.CreateClient();
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var response = await _httpClient.GetAsync("https://localhost:5001/api/Principal");
+            response.EnsureSuccessStatusCode();
+            var resultado = await response.Content.ReadFromJsonAsync<PrincipalRespuesta>();
+
+            return View(resultado.datos);
+        }
+    }
+}
